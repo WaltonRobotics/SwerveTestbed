c3f28d55351a87d8da23fc0a0c6c992488115d36
vmalepati1 <malepativikas@gmail.com>
2022-01-13 17:43:35 -0500

    Add test trajectory

--- SwerveRobot/src/main/java/frc/robot/Robot.java
+++ SwerveRobot/src/main/java/frc/robot/Robot.java
@@ -8,12 +8,15 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.auton.SwerveTrajectoryCommand;
 import frc.robot.commands.teleop.DriveCommand;
 import frc.robot.commands.auton.RotateModulesToAngle;
 import frc.robot.subsystems.Drivetrain;
 
 import static frc.robot.Constants.SmartDashboardKeys.DRIVETRAIN_ROTATE_MODULES_TO_ANGLE_KEY;
 import static frc.robot.Constants.SmartDashboardKeys.DRIVETRAIN_SAVE_CURRENT_AZIMUTH_ZERO_KEY;
+import static frc.robot.Paths.testTrajectory;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -67,6 +70,12 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousInit() {
 //    CommandScheduler.getInstance().schedule(new AutoDrive());
+    drivetrain.reset();
+
+    CommandScheduler.getInstance().schedule(new SequentialCommandGroup(
+            new InstantCommand(() -> drivetrain.resetPose(testTrajectory.getInitialPose())),
+            new SwerveTrajectoryCommand(testTrajectory)
+    ));
   }
 
   /** This function is called periodically during autonomous. */
